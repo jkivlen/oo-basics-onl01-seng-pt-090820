9980fe7c085a0d8053c65566ceec3cfc597c2b8e
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-09-28 03:56:55 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :author, :page_count, :genre
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+  
+end
