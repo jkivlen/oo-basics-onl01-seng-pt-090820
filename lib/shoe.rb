067d7c4e95685b58a6b1be8cbedbf517fc18235e
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-09-28 04:00:17 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,17 +1,8 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor :author, :page_count, :genre, :brand
-  attr_reader :title
+  attr_accessor :brand
   
-  def initialize(title)
-    @title = title
-  end
-  
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
-  
-  
+
   
   
 end
