11af8ce3a04e752a5bcdbd1272b8cd8b1cba8bf7
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-09-28 03:59:36 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,35 +1,10 @@
 class Book
   
+  attr_accessor :author, :page_count, :genre, :brand
+  attr_reader :title
+  
   def initialize(title)
-   @title = title 
-  end
-  
-  def title
-  @title
-  end
-  
-  def author=(author)
-    @author = author
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count=(num)
-    @page_count = num
-  end
-  
-  def page_count
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre
-    @genre
+    @title = title
   end
   
   def turn_page
@@ -39,6 +14,4 @@ class Book
   
   
   
-  
 end
-
